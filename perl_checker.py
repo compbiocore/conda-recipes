5bbd662c9935a23cb5ebd9eacf57bf2863afeea2
August Guang <august.guang@gmail.com>
2018-05-21 00:05:29 -0400

    modified recipes from perl_checker and new ones

--- perl_checker.py
+++ perl_checker.py
@@ -2,11 +2,13 @@
 import os
 import glob
 import yaml
+from jinja2 import Environment, FileSystemLoader
 
 # common issues with the perl recipes for our build system
 # CentOS6, conda-build/2.1.10, python/2.7
 
 # this script is written for python/3.6 however
+# Usage: python perl_checker.py -d $CONDA-RECIPES
 
 # checks for perl-threaded and modifies to just perl
 # if finds, returns 1
@@ -31,7 +33,7 @@ def perl_threaded(meta):
 
 # checks for imports and modifies to commands
 def imports(meta):
-	has_tests = meta['test'].get('imports')
+	has_imports = meta['test'].get('imports')
 	if has_imports:
 		commands = []
 		for i in has_imports:
@@ -45,41 +47,61 @@ def imports(meta):
 
 
 # increments build number if anything has been modified
-def build_number(meta):
-	meta['build']['number']+=1
+def build_number(meta,v):
+	b = meta.get('build')
+	if b:
+		n = b.get('number')
+		if n:
+			meta['build']['number']+=1
+			if v:
+				print("build number is now " + str(meta['build']['number']))
+		elif v:
+			print("no build number...")
+	elif v:
+		print("no build key...")
 
 # checks for test_run.pl and removes
-def test_run(root, files):
+def test_run(root, files, v):
 	if 'run_test.pl' in files:
 		os.remove(os.path.join(root, 'run_test.pl'))
+		if v:
+			print("found run_test.pl and removed")
 
 # run all checks
-def checks(directory):
+def checks(directory, v):
 	perl_recipes = glob.glob(os.path.join(directory,'perl-*'))
-#	perl_recipes = glob.glob(directory)
+#for testing in recipe directory:	perl_recipes = glob.glob(directory)
 	for recipe in perl_recipes:
-		print("checking " + recipe)
+		if opts.verbose: print("------checking " + recipe + "------")
 		for root, dirs, files in os.walk(recipe): # get meta.yaml from every level
-			print(root)
-			print(dirs)
-			print(files)
 			has_recipe = 'meta.yaml' in files
 			if not dirs and has_recipe:
-				with open(os.path.join(recipe,"meta.yaml")) as f:
-					meta = yaml.load(f)
-				print(meta)
+				with open(os.path.join(root,"meta.yaml")) as f:
+					# for Jinja
+					env = Environment(loader=FileSystemLoader(root))
+					template = env.get_template('meta.yaml')
+					meta = yaml.load(template.render())
 
 				threaded = perl_threaded(meta)
+				if v and threaded:
+					print("found perl-threaded")
+
 				if meta.get('test'): # check if there's tests
 					imported = imports(meta)
 				else:
 					imported = 0
-				if threaded or imported: build_number(meta)
-				with open(os.path.join(recipe,"meta.yaml"),"w") as f:
+				if v and imported:
+					print("found import statements")
+
+				if threaded or imported: build_number(meta, v)
+				with open(os.path.join(root,"meta.yaml"),"w") as f:
 					yaml.add_representer(type(None), represent_none)
 					yaml.dump(meta, f, default_flow_style=False)
 
-				test_run(root, files)
+				test_run(root, files, v)
+
+				if v:
+					print("") # for a new line
 
 # from https://stackoverflow.com/questions/37200150/can-i-dump-blank-instead-of-null-in-yaml-pyyaml
 def represent_none(self, _):
@@ -92,7 +114,11 @@ if __name__ == "__main__":
 
     ds = ' [%(default)s]'
     parser.add_argument('-d', '--dir', help='directory with recipes')
+    parser.add_argument('-v', '--verbose', help='verbose')
     opts = parser.parse_args()
 
     directory = opts.dir
-    checks(directory)
+    if opts.verbose:
+    	checks(directory, 1)
+    else:
+    	checks(directory, 0)
