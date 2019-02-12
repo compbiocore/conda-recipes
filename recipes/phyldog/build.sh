ced088eaa533bed5cb96e855f5b70ce107154ae1
August Guang <august.guang@gmail.com>
2019-02-12 17:52:24 -0500

    flattened phyldog recipe dir

--- recipes/phyldog/build.sh
+++ recipes/phyldog/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # libpll libraries
-cp $RECIPE_DIR/include/libpll-1.0.2-sse3-64.tar.gz $SRC_DIR
+cp $RECIPE_DIR/libpll-1.0.2-sse3-64.tar.gz $SRC_DIR
 tar -zxf $SRC_DIR/libpll-1.0.2-sse3-64.tar.gz
 
 mkdir build && cd build
