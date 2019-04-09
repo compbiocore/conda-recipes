afebdeb4d608ff8a5e88814a5612addae1165927
August Guang <august.guang@gmail.com>
2019-04-09 14:32:32 -0400

    closer to working tests

--- recipes/phyldog/build.sh
+++ recipes/phyldog/build.sh
@@ -5,5 +5,5 @@ cp $RECIPE_DIR/libpll-1.0.2-sse3-64.tar.gz $SRC_DIR
 tar -zxf $SRC_DIR/libpll-1.0.2-sse3-64.tar.gz
 
 mkdir build && cd build
-cmake .. -DCMAKE_LIBRARY_PATH="$SRC_DIR/libpll-1.0.2-sse3-64" -DCMAKE_INCLUDE_PATH="$SRC_DIR/libpll-1.0.2-sse3-64/include/" -DBOOST_ROOT="$SRC_DIR/boost"
-make
+cmake .. -DCMAKE_LIBRARY_PATH="$SRC_DIR/libpll-1.0.2-sse3-64" -DCMAKE_INCLUDE_PATH="$SRC_DIR/libpll-1.0.2-sse3-64/include/" -DBOOST_ROOT="$SRC_DIR/boost" -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}"
+make install
